6495cb5a8f2d41d24e518a6efbd698fbac5774ce
rjmiller2543 <MBA11>
2014-08-06 20:51:10 -0400

    added some icons to start bar

--- Swik Prototype/StartBar.m
+++ Swik Prototype/StartBar.m
@@ -16,7 +16,29 @@
     if (self) {
         // Initialization code
         self.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.0 alpha:1.0];
-        self.items = @[[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(testAction)]];
+        self.items = @[ //flex
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
+                        //test 0
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(testAction)],
+                        //flex
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
+                        //test 1
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemBookmarks target:self action:@selector(testAction1)],
+                        //flex
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
+                        //test 2
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward target:self action:@selector(testAction2)],
+                        //flex
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
+                        //test 3
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemOrganize target:self action:@selector(testAction3)],
+                        //flex
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
+                        //test settings
+                       [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"settings.png"] landscapeImagePhone:nil style:UIBarButtonItemStylePlain target:self action:@selector(testSettings)],
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
+                       [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil]
+                       ];
     }
     return self;
 }
@@ -26,6 +48,26 @@
     
     NSLog(@"up testing...");
     
+}
+
+-(void)testAction1
+{
+    NSLog(@"up test 1..");
+}
+
+-(void)testAction2
+{
+    NSLog(@"up test 2..");
+}
+
+-(void)testAction3
+{
+    NSLog(@"up test 3...");
+}
+
+-(void)testSettings
+{
+    NSLog(@"test settings button");
 }
 
 /*
