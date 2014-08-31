0c0e10611e67e9827725cbc39b3d9f4acbc61b4b
rjmiller2543 <MBA11>
2014-08-30 23:14:49 -0400

    Added Colors

---
+++ Swik Prototype/SwikColors.h
@@ -0,0 +1,12 @@
+//
+//  SwikColors.h
+//  Swik Prototype
+//
+//  Created by Robert Miller on 8/30/14.
+//  Copyright (c) 2014 Robert Miller. All rights reserved.
+//
+
+#define BLUE [UIColor colorWithRed:24.0/255.0 green:154.0/255.0 blue:180.0/255.0 alpha:1.0]
+#define DARK_TEAL [UIColor colorWithRed:31.0/255.0 green:167.0/255.0 blue:114.0/255.0 alpha:1.0]
+#define GREEN [UIColor colorWithRed:26.0/255.0 green:190.0/255.0 blue:79.0/255.0 alpha:1.0]
+#define TURUOISE [UIColor colorWithRed:26.0/255.0 green:190.0/255.0 blue:172.0/255.0 alpha:1.0]
--- Swik Prototype/SwikDefinitions.h
+++ Swik Prototype/SwikDefinitions.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Robert Miller. All rights reserved.
 //
 
+#include "SwikColors.h"
+
 #ifndef Swik_Prototype_SwikDefinitions_h
 #define Swik_Prototype_SwikDefinitions_h
 
